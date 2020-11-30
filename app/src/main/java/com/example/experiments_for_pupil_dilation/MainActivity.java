0508d67645e693670475c06378e5bff8f3c407a8
Mr.Finch <gorkemtemizell@gmail.com>
2020-12-01 00:21:28 +0300

    Experiment Selector ready to go.

---
+++ app/src/main/java/com/example/experiments_for_pupil_dilation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.experiments_for_pupil_dilation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
